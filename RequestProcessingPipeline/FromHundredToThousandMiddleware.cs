a545e0a82abea196c0027084642ec29587a21cb5
Alex Yelienok <aelenok@gmail.com>
2024-06-17 22:30:37 +0200

    Add project files.

---
+++ RequestProcessingPipeline/FromHundredToThousandMiddleware.cs
@@ -0,0 +1,44 @@
+﻿namespace RequestProcessingPipeline
+{
+    public class FromHundredToThousandMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FromHundredToThousandMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string? token = context.Request.Query["number"];
+            try
+            {
+                int number = Convert.ToInt32(token);
+                number = Math.Abs(number);
+                if (number >= 100 && number <= 999)
+                {
+                    string[] Hundreds = { "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred" };
+                    if (number % 100 == 0)
+                    {
+                        await context.Response.WriteAsync("Your number is " + Hundreds[number / 100 - 1]);
+                    }
+                    else
+                    {
+                        context.Session.SetString("number", (number % 100).ToString());
+                        context.Session.SetString("hundreds", Hundreds[number / 100 - 1]);
+                        await _next(context);
+                    }
+                }
+                else
+                {
+                    await _next(context);
+                }
+            }
+            catch (Exception)
+            {
+                await context.Response.WriteAsync("Incorrect parameter");
+            }
+        }
+    }
+}
